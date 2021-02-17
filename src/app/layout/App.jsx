cef57e7e7e52d7960de133febfa6b20c42c0e3a1
ridwan57 <ridwananik57@gmail.com>
2021-02-17 22:18:30 +0600

    initaila commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <div>
+        <h1></h1>
+
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
